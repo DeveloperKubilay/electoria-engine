4b41aac5399e8f0619d1c954de107cb12eab756a
DeveloperKubilay <107062509+DeveloperKubilay@users.noreply.github.com>
2023-07-17 23:46:29 +0300

    Create onload.js

---
+++ onload.js
@@ -0,0 +1,12 @@
+Engine_onload = 
+{
+    fps:"auto",//you can select "75" or auto = 250
+    onlyonscreen:true,//rendering off screen
+    splitelimit:"2000",//sprites on max screen
+    backgroundspeed:"3000",//background change rate
+    gravity:"0.3",
+    yfriction:"0.4",
+    xfriction:"0.08",
+    forcepresstime:"60",//keypress fps
+    autoshaders:true//something like the shadow of the sun
+}

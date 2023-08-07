1b858af2129c378e160da3c76de61f98fb77c31d
DeveloperKubilay <107062509+DeveloperKubilay@users.noreply.github.com>
2023-08-07 04:24:12 +0300

    Update script.js

--- docs/demoGames/2d_pingpong/script.js
+++ docs/demoGames/2d_pingpong/script.js
@@ -1,9 +1,7 @@
-setTimeout(()=>{
-  editdisplay(1024,720)
+editdisplay(1024,720)
 
 
-var fast = 50;
-var maxy = 25;
+var fast = 10;
 var ease = 20;
 
 
@@ -13,11 +11,11 @@ var player1 = new Component({
     x:100,y:0
   },
   scale:{
-    x:25,y:130
+    x:25,y:150
   },
   inscreen:true,
   color:"red",
-  collision:2
+  collision:1
 })
 
 
@@ -28,10 +26,10 @@ var bot = new Component({
   },
   
   scale:{
-    x:25,y:50
+    x:25,y:150
   },
   color:"green",
-  collision:2
+  collision:1
 })
 
 
@@ -56,27 +54,27 @@ var ball = new Component({
 
 keypress = ((key)=>{
   if(key === "W") {
-    player1.update({position:{y:"-20"}})
+    player1.update({position:{y:"-80"}})
   }else if(key === "S"){
-    player1.update({position:{y:"+20"}})
+    player1.update({position:{y:"+80"}})
   }
   else if(key === "ARROWUP"){
-    player1.update({position:{y:"-20"}})
+    player1.update({position:{y:"-80"}})
 
   }else if(key === "ARROWDOWN"){
-    player1.update({position:{y:"+20"}})
+    player1.update({position:{y:"+80"}})
   }
 })
 
 mousescroll = function(type){
  if(type == "down"){
-  player1.update({position:{y:"+40"}})
+  player1.update({position:{y:"+80"}})
  }else{
-  player1.update({position:{y:"-40"}})
+  player1.update({position:{y:"-80"}})
  }
 } 
 
-var moveball = {y:0,x:fast}
+var moveball = {y:2,x:fast}
 setInterval(()=>{
   if(ball.get().position.y < 20){
     ball.update({position:{y:20}})
@@ -87,13 +85,14 @@ setInterval(()=>{
   if(moveball.hasOwnProperty("y")){
     ball.update({physic:{y:moveball.y}})
   }
-},32)
+},10)
 
 
 setInterval(()=>{
-  bot.update({
+if(ball.get().scale.y == ball.get().position.y)moveball.y = -moveball.y
+bot.update({
     position:{
-      y:Math.max(1, Math.min(620, ball.get().position.y))
+      y:Math.max(1, Math.min(Engine_canvas.height-bot.get().scale.y, ball.get().position.y))
     }
   })
 },ease)
@@ -101,37 +100,39 @@ setInterval(()=>{
 collisionout = ((data,z,location,fside)=>{
  if(data == "ball"){
     if(location == "right") moveball.x = fast; else moveball.x = -fast;
-    if(fside.dy > 0){
-      moveball.y = Math.max(-1, Math.min(-maxy, fside.dy));
-    }else{
-      moveball.y = Math.max(1, Math.min(-maxy, fside.dy));
-    }
+      moveball.y =((100*fside.dy)/-fside.height)/10
  }
 })
 
 physicout= ((data,z)=>{
   if(z.left){
     stopgame()
-    setTimeout(()=>reloadgame(),2000) 
-
+   setTimeout(()=>reloadgame(),1500) 
+    sendengine({
+      sendvibrate:200,
+      changestatusbar:"#ff0000"
+    })
   }else if(z.right){
     stopgame()
-    setTimeout(()=>reloadgame(),2000) 
-
+    setTimeout(()=>reloadgame(),1500) 
+    sendengine({
+      sendvibrate:200,
+      changestatusbar:"#ff0000"
+    })
   }else if(z.top){
     moveball.y = -moveball.y
 
   }else if(z.bottom){
     moveball.y = -moveball.y
-
   }
 })
-
 
 var latesttouch = 1
 touchmove = function(data){
   data = data[0]
-  console.log((data.y-latesttouch).toFixed(0))
  player1.update({position:{y:data.y-latesttouch}})
 }
-},200)
+sendengine({
+  changescreenstatus:3,
+  changestatusbar:"#1e1e1e"
+})

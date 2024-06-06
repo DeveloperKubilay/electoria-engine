1d4331ec779ddfa21736599b5620c40ec3d75bcf
Kubilay <107062509+DeveloperKubilay@users.noreply.github.com>
2024-06-06 15:31:09 +0300

    Update onload.js

--- onload.js
+++ onload.js
@@ -10,6 +10,6 @@ Engine_onload = //https://electoriaengine.gitbook.io/api/engine/onload
     yfriction:"0.5",//default physics settings
     xfriction:"0.08",//default physics settings
     forcepresstime:"60",//keypress fps
-    gamepadpressinterval:"1000",//gamepad update speed
+    gamepadpressinterval:"30",//gamepad update speed
     autoshaders:60000//something like the shadow of the sun,If you type 0 it closes,you enter in-game time
 }
